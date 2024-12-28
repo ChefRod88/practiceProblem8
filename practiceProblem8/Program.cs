a664a7cfa9e3d4359db1aea90c330b95ab63f9a7
Rodney Chery <rchery7@wgu.edu>
2024-12-28 10:52:17 -0500

    Initial commit

---
+++ practiceProblem8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        // List of frameworks
+        var frameworks = new List<string> { "Django", "Flask", "CherryPy", "Bottle", "Web2Py", "TurboGears" };
+
+        // Accept integer input for the index
+        int I;
+        if (int.TryParse(Console.ReadLine(), out I))
+        {
+            try
+            {
+                // Try to access the element in the list at index I
+                string V = frameworks[I];
+                // Output the corresponding framework name if index is valid
+                Console.WriteLine(V);
+            }
+            catch (ArgumentOutOfRangeException)
+            {
+                // If the index is out of range, print "Error"
+                Console.WriteLine("Error");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Error");
+        }
+    }
+}
